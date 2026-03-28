17ded5fbf8a8363183bd2992ad2fa723e178398b
jay <exx.jj23@gmail.com>
2026-03-28 14:18:03 -0600

    added player data shell

--- san-life/scripts/PlayerData.gd
+++ san-life/scripts/PlayerData.gd
@@ -1,11 +1,20 @@
 extends Node
 
+#Identity
+var player_name: String = ""
+var is_female: bool = false
 
-# Called when the node enters the scene tree for the first time.
-func _ready() -> void:
-	pass # Replace with function body.
+
+#Appearance
+var skin_color: Color = Color(1.0, 0.85, 0.7)
+var eye_color: Color = Color(0.2, 0.4, 0.8)
+var hair_index: int = 0
+var hair_color: Color = Color(0.3, 0.15, 0.05)
 
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	pass
+#Clothing
+var shirt_index: int = 0
+var pants_index: int = 0
+var shoes_index: int = 0
+var hat_index: int = 0
+var phone_case_index: int = 0
