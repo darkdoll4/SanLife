7bbdd8cbcf5b6e2a7cc32af4715bb2bfcf12dcdf
jay <exx.jj23@gmail.com>
2026-03-27 18:16:30 -0600

    all buttons functioning

--- san-life/scenes/main_menu.gd
+++ san-life/scenes/main_menu.gd
@@ -8,3 +8,11 @@ func _on_quit_pressed() -> void:
 
 func _on_new_game_pressed() -> void:
 	get_tree().change_scene_to_file("res://scenes/character_creator.tscn")
+
+
+func _on_continue_pressed() -> void:
+	print("continue pressed")
+
+
+func _on_settings_pressed() -> void:
+	print("settings pressed")
